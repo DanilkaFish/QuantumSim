81dfc84e7dfba21b78a6dfc3a0020fb9c60243b7
danilkafish <fisher.de19@physics.msu.ru>
2024-10-19 19:51:57 +0300

    Deleting redundant files

--- main.cpp
+++ main.cpp
@@ -51,47 +51,11 @@ int main(){
         ia2[i] = n + i;
     }
     tensor::State st1(n - 2);
-    // std::cout << tensor::State(n - 2);
     tensor::State s = tensor::statedot(st1, st, ia, ia2);
     std::cout << s.get_n_qubits() << std::endl;
     std::cout << s.get_array().size() << std::endl;
     std::cout << s;
     std::cout << s.norm();
-    // int n_qubits = 5;
-    // std::vector<tensor::State> vs;
-    // for (int i=0; i< n_qubits; i++){
-    //     vs.push_back(tensor::State(1));
-    // }
-    // tensor::State s3(vs);
-    // std::cout << " toto: " <<  s3;
+
 }
 
-// int main(){
-//     int seed = 1818;
-//     QCGenerator obj{128, 6, "LCG", seed};
-//     QuantumCircuit toto = obj.get_qc(0.64);
-//     // tensor::HSMatrix a(ComplexArr{1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0});
-//     // tensor::HSMatrix prod(a*a);
-//     // std::cout << prod;
-//     // Instruction cx{tensor::HSMatrix(ComplexArr{1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0}), IntArr{0,1}};
-//     Instruction hx{tensor::HSMatrix(ComplexArr{0,1,0,1,1,0,1,0,0,1,0,-1,1,0,-1,0}), IntArr{0,1}};
-//     // tensor::i
-//     using tensor::i;
-//     Instruction hy{tensor::HSMatrix(ComplexArr{0,-i,0,-i,i,0,i,0,0,-i,0,i,i,0,-i,0}), IntArr{0,1}};
-//     std::cout << hy.un << std::endl;
-//     hy.inverse();
-//     std::cout << hy.un << std::endl;
-    
-//     std::cout << toto.get_qc_data().size()<< std::endl;
-//     tensor::State s = toto.execute();
-//     std::cout << "cm : " << cumm_sum(s) << std::endl; 
-//     toto.simplification();
-//     s = toto.execute();
-//     std::cout << "cm : " << cumm_sum(s) << std::endl; 
-//     // // tensor::State s = toto.s;
-//     // std::cout << "psi = " << s << std::endl;
-//     // std::cout << "norm : " << s.norm()  << std::endl;
-//     // tensor::HSMatrix H(ComplexArr{0,1,1,0});
-//     // tensor::HSMatrix U = H.to_unitary();
-
-// }
